c84b86890a511ad8846a4d92124320d878cf194f
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2024-03-15 08:27:14 -0700

    refactor: lint all js files

--- src/App.js
+++ src/App.js
@@ -1,89 +1,132 @@
-import React, { useState, useEffect }from 'react';
-import Header from './components/Header';
-import PokeSprite from './components/PokeSprite'
-import PokemonSelector from './components/PokemonSelector'
-import axios from 'axios'
+import React, {useState, useEffect} from "react";
+import Header from "./components/Header";
+import PokeSprite from "./components/PokeSprite";
+import PokemonSelector from "./components/PokemonSelector";
+import axios from "axios";
 
-
+/**
+ * The main app component
+ * @module App
+ * @return {React.Component} The main app component
+ */
 function App() {
+    const [pokemonData, setPokemonData] = useState();
+    const [pokemons, setPokemons] = useState([]);
+    const [fullPokeList, setFullPokeList] = useState([]);
+    let firstLoaded = false;
 
-  const [pokemonData, setPokemonData] = useState()
-  const [pokemons, setPokemons] = useState([])
-  const [fullPokeList, setFullPokeList] = useState([]);
-  var firstLoaded = false;
+    useEffect(() => {
+        if (firstLoaded) return;
+        firstLoaded = true;
+        axios.get("https://pokeapi.co/api/v2/pokemon/?limit=2000").then((res) =>
+            res.data.results.map((pokemon) => {
+                setFullPokeList((oldPokeList) => [
+                    ...oldPokeList,
+                    pokemon.name,
+                ]);
+            }),
+        );
+        loadPokeData("vaporeon");
+    }, []);
 
-  useEffect(()=>{
-    if (firstLoaded) return;
-    firstLoaded = true;
-    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=2000").then(res => 
-      res.data.results.map(pokemon => {
-        setFullPokeList(oldPokeList => [...oldPokeList, pokemon.name]); 
-      }))
-    loadPokeData("vaporeon")
-  }, [])
-
-  useEffect(()=>{
-    if (pokemonData) setPokemons(oldPokeList => [...oldPokeList, pokemonData])
-  }, [pokemonData])
-
-  function loadPokeData(pokeName) {
-    axios.get("https://pokeapi.co/api/v2/pokemon/" + pokeName).then(res => setPokemonData(prevPokeData => res.data))
-  }
-
-  function submitClicked() {
-    let input = document.getElementById('selector')
-    let processedName = processPokemonName(input.value)
-    if (fullPokeList.includes(processedName)){
-      loadPokeData(processedName)
-      input.value=''
-      if (input.className === "error") input.className = ""
-      return
-    }
-    input.className = "error"
-  }
-  
-  // converts complex names like "Mega Charizard X" to "charizard-mega-x" so the api can handle it
-  function processPokemonName(pokemonName) {
-    pokemonName = pokemonName.toLowerCase()
-    let words = pokemonName.split(' ')    
-    // 3 possibilities
-    // 1 word, such as "vaporeon"
-    // 2 words, such as "gigantamax flapple"
-    // 3 words, such as "mega charizard x"
-    switch (words[0]) {
-      case "gigantamax": words[0] = "gmax"; break
-      case "alolan": words[0] = "alola"; break
-    }
-
-    switch (words.length) {
-      case 1: return words[0]
-      case 2: return words[1] + "-" + words[0]
-      case 3: return words[1] + "-mega-" + words[2]
-      // unprocessable names
-      default: return pokemonName
-    }
-  }
-
-  // array splicing not causing a rerender so we need to figure out a different way to delete pokemon onclick
-  function selfTerminate(index){
-    setPokemons(oldMons=>{
-      let left = oldMons.slice(0, index)
-      let right = oldMons.slice(index+1)
-      return [...left, ...right]
-    })
-  }
-
-  return (
-    <div className="App">
-      <Header />
-      <PokemonSelector submitName={submitClicked}/>
-      <div id="conga-wrapper">
-        {
-          pokemons.map((pokemon, index) => <PokeSprite pokeData={pokemon} selfTerminate={() => {selfTerminate(index)}}/>)
+    useEffect(() => {
+        if (pokemonData) {
+            setPokemons((oldPokeList) => [...oldPokeList, pokemonData]);
         }
-      </div>
-    </div>
-  );
+    }, [pokemonData]);
+
+    /**
+     * Loads the data for a pokemon into the pokemonData state
+     * @param {string} pokeName The name of the pokemon to load
+     * @return {void}
+     */
+    function loadPokeData(pokeName) {
+        axios
+            .get("https://pokeapi.co/api/v2/pokemon/" + pokeName)
+            .then((res) => setPokemonData((prevPokeData) => res.data));
+    }
+
+    /**
+     * Handles the submit button being clicked
+     * Processes the input and loads the pokemon data
+     * @return {void}
+     */
+    function submitClicked() {
+        const input = document.getElementById("selector");
+        const processedName = processPokemonName(input.value);
+        if (fullPokeList.includes(processedName)) {
+            loadPokeData(processedName);
+            input.value = "";
+            if (input.className === "error") input.className = "";
+            return;
+        }
+        input.className = "error";
+    }
+
+    /**
+     * Processes a pokemon name to be used in the API
+     * @param {string} pokemonName The name of the pokemon to process
+     * @return {string} The processed name
+     *
+     * @example
+     * processPokemonName("Mega Charizard X") // returns "charizard-mega-x"
+     * processPokemonName("Gigantamax Flapple") // returns "flapple-gmax"
+     * processPokemonName("Alolan Raichu") // returns "raichu-alola"
+     */
+    function processPokemonName(pokemonName) {
+        pokemonName = pokemonName.toLowerCase();
+        const words = pokemonName.split(" ");
+        switch (words[0]) {
+        case "gigantamax":
+            words[0] = "gmax";
+            break;
+        case "alolan":
+            words[0] = "alola";
+            break;
+        }
+
+        switch (words.length) {
+        case 1:
+            return words[0];
+        case 2:
+            return words[1] + "-" + words[0];
+        case 3:
+            return words[1] + "-mega-" + words[2];
+            // unprocessable names
+        default:
+            return pokemonName;
+        }
+    }
+
+    /**
+     * Removes a pokemon from the list
+     * @param {number} index The index of the pokemon to remove
+     */
+    function selfTerminate(index) {
+        setPokemons((oldMons) => {
+            const left = oldMons.slice(0, index);
+            const right = oldMons.slice(index + 1);
+            return [...left, ...right];
+        });
+    }
+
+    return (
+        <div className="App">
+            <Header />
+            <PokemonSelector submitName={submitClicked} />
+            <div id="conga-wrapper">
+                {pokemons.map((pokemon, index) => (
+                    <PokeSprite
+                        key={index}
+                        pokeData={pokemon}
+                        selfTerminate={() => {
+                            selfTerminate(index);
+                        }}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/PokeSprite.js
+++ src/components/PokeSprite.js
@@ -1,45 +1,79 @@
-import React from 'react'
-import './PokeSprite.css'
+import React from "react";
+import "./PokeSprite.css";
 
+/**
+ * The sprite of a pokemon
+ * @module PokeSprite
+ * @param {Object} pokeData The data of the pokemon
+ * @param {function} selfTerminate The deletion function
+ *
+ * @return {React.Component} The sprite of a pokemon
+ */
 export default function PokeSprite( {pokeData, selfTerminate} ) {
+    PokeSprite.propTypes = {
+        pokeData: Object,
+        pokeData: {
+            name: String,
+            height: Number,
+            sprites: Object,
+            front_default: String,
+        },
+        selfTerminate: Function,
+    };
     const pokeStyle = {
         height: pokeData.height*10 + "px",
-    }
+    };
 
     /**
      * Reformats pokemon name from API to be more readable
-     * @param {string} name 
-     * @returns reformatted pokemon name
-     *  - Capitalizes first letter
-     *  - Removes dashes
-     *  - Adds "Galarian" or "Alolan" to front of name
-     *  - Adds "Mega" to the front and "X" or "Y" to back of the name
-     *  - Adds "Gigantamax" to front of name
+     * @param {string} name
+     * @return {string} The reformatted name
      */
-
     const pokeName = (name) => {
         if (name.endsWith("-mega")) {
-            return "Mega " + name.charAt(0).toUpperCase() + name.slice(1, -5)
+            return "Mega " + name.charAt(0).toUpperCase() + name.slice(1, -5);
         } else if (name.endsWith("-alola")) {
-            return "Alolan " + name.charAt(0).toUpperCase() + name.slice(1, -6)
+            return "Alolan " + name.charAt(0).toUpperCase() + name.slice(1, -6);
         } else if (name.endsWith("-galar")) {
-            return "Galarian " + name.charAt(0).toUpperCase() + name.slice(1, -6)
+            return (
+                "Galarian " + name.charAt(0).toUpperCase() + name.slice(1, -6)
+            );
         } else if (name.endsWith("-gmax")) {
-            return "Gigantamax " + name.charAt(0).toUpperCase() + name.slice(1, -5)
+            return (
+                "Gigantamax " + name.charAt(0).toUpperCase() + name.slice(1, -5)
+            );
         } else if (name.endsWith("-mega-x")) {
-            return "Mega " + name.charAt(0).toUpperCase() + name.slice(1, -7) + " X"
+            return (
+                "Mega " +
+                name.charAt(0).toUpperCase() +
+                name.slice(1, -7) +
+                " X"
+            );
         } else if (name.endsWith("-mega-y")) {
-            return "Mega " + name.charAt(0).toUpperCase() + name.slice(1, -7) + " Y"
+            return (
+                "Mega " +
+                name.charAt(0).toUpperCase() +
+                name.slice(1, -7) +
+                " Y"
+            );
         } else {
-            return name.charAt(0).toUpperCase() + name.slice(1)
+            return name.charAt(0).toUpperCase() + name.slice(1);
         }
-    }
+    };
 
     return (
         <div className="poke-sprite" onClick={selfTerminate}>
-            <p className="sprite-info">{pokeName(pokeData.name)}
-            <br />{pokeData.height/10}m</p>
-            <img className="poke-img" src={pokeData.sprites.front_default} alt={pokeData.name} style={pokeStyle}/>
+            <p className="sprite-info">
+                {pokeName(pokeData.name)}
+                <br />
+                {pokeData.height / 10}m
+            </p>
+            <img
+                className="poke-img"
+                src={pokeData.sprites.front_default}
+                alt={pokeData.name}
+                style={pokeStyle}
+            />
         </div>
-    )
+    );
 }
